df196394ec41af8110684083364d2558355e9ceb
Isaac Willian <isaaacwillian@gmail.com>
2021-12-23 13:54:38 -0300

    Troquei a bola por um quadrado que muda de cores ao bater na parede

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Bola batendo na parede
+# Quadrado batendo na parede
 Projeto utilizando o canvas do HTML
 - [HTML]
 - [Javascript]
 <h1 align="left">
-    <img alt="Este é um Gif mostrando a bola batendo na parede e voltando" src="readme/bola-batendo.gif">
+    <img alt="Este é um Gif mostrando a bola batendo na parede e voltando" src="readme/square.gif">
 </h1>
--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 let tela = document.getElementsByTagName("canvas")[0].getContext("2d");
 
-let circle = {
+let square = {
     x: 50,
     y: 50,
-    raio: 50,
-    beginning: 0,
-    ending: 2 * Math.PI
 }
-function drawCircle(c) {
+function drawSquare(s) {
 
     tela.beginPath();
     tela.rect(0, 0, 500, 500);
@@ -15,7 +12,11 @@ function drawCircle(c) {
     tela.fill();
 
     tela.beginPath();
-    tela.arc(c.x, c.y, c.raio, c.beginning, c.ending);
+    tela.moveTo(s.x, s.y);
+    tela.lineTo(s.x + 50, s.y)
+    tela.lineTo(s.x + 50, s.y + 50)
+    tela.lineTo(s.x, s.y + 50)
+    tela.lineTo(s.x, s.y)
     tela.strokeStyle = "red";
     tela.fillStyle = "blue";
     tela.fill();
@@ -26,22 +27,39 @@ let b = 1;
 let c = 0;
 let d = 1;
 setInterval(function () {
-    drawCircle(circle);
+    drawSquare(square);
     if (a > 450) {
         b = -2;
     }
-    if (a < 50) {
+    if (a < 0) {
         b = 3;
     }
     a += b;
     if (c > 450) {
         d = -1;
+
     }
-    if (c < 50) {
+    if (c < 0) {
         d = 2;
     }
+    if (b == -2 && d == -1) {
+        tela.fillStyle = "red";
+        tela.fill();
+    }
+    if (b == 3 && d == 2) {
+        tela.fillStyle = "green";
+        tela.fill();
+    }
+    if (d == -1 && b == 3) {
+        tela.fillStyle = "blue";
+        tela.fill();
+    }
+    if (d == 2 && b == -2) {
+        tela.fillStyle = "yellow";
+        tela.fill();
+    }
     c += d;
-    circle.x = a;
-    circle.y = c;
+    square.x = a;
+    square.y = c;
 })
 
